f0c3a8f8865da8ca40d8dff9ab5efc53d948abcb
Spike Washburn <swashburn@cloudbees.com>
2017-11-07 10:50:28 -0500

    another comment change

--- src/main/java/example/rest/Word.java
+++ src/main/java/example/rest/Word.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 import org.codehaus.jackson.annotate.JsonProperty;
 
-//a comment
 public class Word implements Serializable {
     @JsonProperty
     public String id;
